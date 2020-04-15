efdec41686c41ea3753cafc6006ef05dda88dac6
wxr007 <wxr_007@qq.com>
2020-04-15 22:50:03 +0800

    python test

---
+++ spider_test1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# 导入urllib2 库
+import urllib.request
+
+# 使用urllib2.urlopen()向指定的url发送请求，并返回服务器响应的类文件对象
+response = urllib.request.urlopen("https://www.jdlingyu.mobi/tag/%E5%96%B5%E7%B3%96%E6%98%A0%E7%94%BB")
+
+# 类文件对象的read()方法可读取文件全部内容，返回字符串
+html = response.read()
+
+f = open("new.html","wb")
+# 打印字符串（页面源码）
+#print(html)
+f.write(html)
---
+++ spider_test2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 导入urllib2 库
+import urllib.request
+
+# url 作为Request()方法的参数，构造并返回一个Request对象
+request = urllib.request.Request("https://www.jdlingyu.mobi/tag/%E5%96%B5%E7%B3%96%E6%98%A0%E7%94%BB")
+
+# Request对象作为urlopen()方法的参数，发送给服务器并接收响应
+response = urllib.request.urlopen(request)
+
+html = response.read()
+
+f = open("new.html","wb")
+# 打印字符串（页面源码）
+#print(html)
+f.write(html)
